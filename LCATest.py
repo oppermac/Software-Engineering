7f4d24c4c4c321cbf907b4f2f6f9e015ee2b1977
oppermac <72154267+oppermac@users.noreply.github.com>
2020-10-27 19:46:50 +0000

    Update LCATest.py

--- LCATest.py
+++ LCATest.py
@@ -14,11 +14,9 @@ class TestLCA(unittest.TestCase):
         root.left.right.left = Node(10)
         root.left.right.right = Node(14)
 
-        n1 = 10
-        n2 = 14
-        assert lca(root, n1, n2).data == 12
+        assert lca(root, 10, 14).data == 12
 
-    def test_lca_output_2(self):
+    def test_lca_output_two(self):
         root = Node(15)
         root.left = Node(12)
         root.left.left = Node(11)
@@ -26,9 +24,7 @@ class TestLCA(unittest.TestCase):
         root.right = Node(18)
         root.right.left = Node(17)
 
-        n1 = 17
-        n2 = 13
-        assert lca(root, n1, n2).data == 15
+        assert lca(root, 17, 13).data == 15
 
     def test_tree_to_string(self):
         root = Node(20)
@@ -45,18 +41,14 @@ class TestLCA(unittest.TestCase):
         assert ''.join(string) == '20(8(4)(12(10)(14)))(22)'
 
     def test_LCA_none_node(self):
-        n1 = 1
-        n2 = 3
-
-        assert lca(None, n1, n2) is None
+       
+        assert lca(None, 1, 3) is None
 
     def test_LCA_single_node_tree(self):
 
         root = Node(1)
-        n1 = 1
-        n2 = 3
 
-        assert lca(root, n1, n2).data == 1
+        assert lca(root, 1, 3).data == 1
 
     def test_arguments_not_in_tree(self):
 
@@ -64,10 +56,7 @@ class TestLCA(unittest.TestCase):
         root.left = Node(1)
         root.right = Node(3)
 
-        n1 = 5
-        n2 = 6
-
-        assert lca(root, n1, n2) is None
+        assert lca(root, 5, 6) is None
 
     def test_empty_tree(self):
 
@@ -84,6 +73,7 @@ class TestLCA(unittest.TestCase):
         root.right.right = Node(60)
 
         assert lca(root, 60, 40).data == 50
-        assert lca(root, 30, 40).data == 30
         assert lca(root, 60, 20).data == 50
         assert lca(root, 70, 20).data == 50
+        assert lca(root, 30, 40).data == 30
+       
